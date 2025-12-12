f5069b8817661b1632f93d5e5a6d30b214fc9511
bakabaka9405 <2677423642@qq.com>
2025-12-12 19:07:48 +0800

    fix: 修复 publicPath

--- farm.config.ts
+++ farm.config.ts
@@ -5,6 +5,9 @@ import { resolve } from 'path';
 export default defineConfig({
   vitePlugins: [vue()],
   compilation: {
+    output: {
+      publicPath: './'
+    },
     resolve: {
       alias: {
         '@': resolve(__dirname, 'src')
